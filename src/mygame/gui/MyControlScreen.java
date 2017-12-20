1b9bea2ccb58af5e4eb4bf8d2f8d7d20d1ed4ded
maximilian <2085099m@student.gla.ac.uk>
2017-12-20 21:42:16 +0000

    soc

--- src/mygame/gui/MyControlScreen.java
+++ src/mygame/gui/MyControlScreen.java
@@ -8,9 +8,6 @@ package mygame.gui;
 import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
 import com.jme3.scene.Spatial;
 import de.lessvoid.nifty.Nifty;
@@ -19,6 +16,7 @@ import de.lessvoid.nifty.screen.ScreenController;
 import de.lessvoid.nifty.screen.Screen;
 import mygame.Project;
 import mygame.ResourceLoader;
+import mygame.states.GuiAppState;
 
 /**
  * Manages the main GUI
@@ -28,23 +26,19 @@ import mygame.ResourceLoader;
 public class MyControlScreen extends AbstractAppState implements ScreenController {
     Nifty nifty;
     Screen screen;
-    
-    private ResourceLoader loader;
-    private Project app;
-    private Camera flyCam;
-            
+    private GuiAppState gui;
+    private Project app;      
     private float altitude;
-   
+    
+    public MyControlScreen(GuiAppState gui){
+        this.gui = gui;
+    }
+    
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
         
         this.app=(Project) app;
-        this.flyCam = this.app.getCamera();
-        this.loader = this.app.getResourceLoader();
-        
-        //TODO: initialize your AppState, e.g. attach spatials to rootNode
-        //this is called on the OpenGL thread after the AppState has been attached
     }
     
     @Override
@@ -76,55 +70,37 @@ public class MyControlScreen extends AbstractAppState implements ScreenControlle
     }
     
     public void frontView() {
-         Quaternion rotation = new Quaternion();
-        // rotate 170 degrees around y axis
-        rotation.fromAngleAxis( FastMath.PI , new Vector3f(0,1,0) );
-        flyCam.setRotation(rotation);
-        flyCam.setLocation( new Vector3f( 0.08276296f, 15.758865f+altitude, 337.568f ) );
+        gui.frontView();
      }
 
     
     public void aboveView() {
-        Quaternion rotation = new Quaternion();
-        // rotate 90 degrees around x axis
-        rotation.fromAngleAxis( FastMath.PI/2 , new Vector3f(1,0,0) );
-        flyCam.setRotation(rotation);
-        flyCam.setLocation( new Vector3f( -0.42916974f, 356.08267f+altitude, 79.266045f ) ); 
+        gui.aboveView();
      }
     
     public void rightEngineView() {
-        Quaternion rotation = new Quaternion();
-        // rotate 5/4*pi around y axis
-        rotation.fromAngleAxis((float) (FastMath.PI * 0.75), new Vector3f(0,1,0) );
-        flyCam.setRotation(rotation);
-        flyCam.setLocation( new Vector3f(-233.71786f, 29.250921f+altitude, 249.49205f));
+        gui.rightEngineView();
      }
     
     public void leftEngineView() {  
-        Quaternion rotation = new Quaternion();
-        // rotate 5/4*pi around y axis
-        rotation.fromAngleAxis((float) (FastMath.PI * 1.25), new Vector3f(0,1,0) );
-        flyCam.setRotation(rotation);
-        flyCam.setLocation( new Vector3f(233.71786f, 29.250921f+altitude, 249.49205f));
+        gui.leftEngineView();
      }
     
     public void submit(){
-        moveAircraft();
-    }
-    
-    
-    public void moveAircraft(){
         screen = nifty.getCurrentScreen();
 
         TextField altitudeField = screen.findNiftyControl("altitudeField", TextField.class);        
         String altitudeString = altitudeField.getRealText();
         altitude = Integer.parseInt(altitudeString);
         
-        Spatial aircraft = this.app.getRootNode().getChild("3d-model-objnode");
+         Spatial aircraft = this.app.getRootNode().getChild("3d-model-objnode");
         
         aircraft.setLocalTranslation(0, altitude, 0);
         
+        gui.setVariables((int) altitude);
     }
+    
+
     
     
     public void quitGame() {
--- src/mygame/states/GuiAppState.java
+++ src/mygame/states/GuiAppState.java
@@ -8,9 +8,12 @@ package mygame.states;
 import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
 import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.renderer.Camera;
 import de.lessvoid.nifty.Nifty;
-import mygame.Project;
 import mygame.Project;
 import mygame.gui.MyControlScreen;
 
@@ -22,20 +25,27 @@ import mygame.gui.MyControlScreen;
 public class GuiAppState extends AbstractAppState {
     
     private Project app;
+    
     private NiftyJmeDisplay niftyDisplay;
+    private Nifty nifty;
+    
     private MyControlScreen controlScreen;
+    private Camera flyCam;
+    
+    private int altitude;
     
     @Override
     public void initialize(AppStateManager stateManager, Application app) {
         super.initialize(stateManager, app);
         
         this.app = (Project) app;
+        this.flyCam = this.app.getCamera();
         
         niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(app.getAssetManager(), app.getInputManager(), app.getAudioRenderer(), app.getGuiViewPort());
-        Nifty nifty = niftyDisplay.getNifty();
+        nifty = niftyDisplay.getNifty();
         
         /** Read your XML and initialize your custom ScreenController */
-        controlScreen = new MyControlScreen();
+        controlScreen = new MyControlScreen(this);
         stateManager.attach(controlScreen);
         
         nifty.fromXml("Interface/screen.xml", "start", controlScreen);
@@ -57,5 +67,43 @@ public class GuiAppState extends AbstractAppState {
         //e.g. remove all spatials from rootNode
         //this is called on the OpenGL thread after the AppState has been detached
     }
+
+    public void frontView() {
+         Quaternion rotation = new Quaternion();
+        // rotate 170 degrees around y axis
+        rotation.fromAngleAxis( FastMath.PI , new Vector3f(0,1,0) );
+        flyCam.setRotation(rotation);
+        flyCam.setLocation( new Vector3f( 0.08276296f, 15.758865f+altitude, 337.568f ) );
+     }
+
+    
+    public void aboveView() {
+        Quaternion rotation = new Quaternion();
+        // rotate 90 degrees around x axis
+        rotation.fromAngleAxis( FastMath.PI/2 , new Vector3f(1,0,0) );
+        flyCam.setRotation(rotation);
+        flyCam.setLocation( new Vector3f( -0.42916974f, 356.08267f+altitude, 79.266045f ) ); 
+     }
+    
+    public void rightEngineView() {
+        Quaternion rotation = new Quaternion();
+        // rotate 5/4*pi around y axis
+        rotation.fromAngleAxis((float) (FastMath.PI * 0.75), new Vector3f(0,1,0) );
+        flyCam.setRotation(rotation);
+        flyCam.setLocation( new Vector3f(-233.71786f, 29.250921f+altitude, 249.49205f));
+     }
+    
+    public void leftEngineView() {  
+        Quaternion rotation = new Quaternion();
+        // rotate 5/4*pi around y axis
+        rotation.fromAngleAxis((float) (FastMath.PI * 1.25), new Vector3f(0,1,0) );
+        flyCam.setRotation(rotation);
+        flyCam.setLocation( new Vector3f(233.71786f, 29.250921f+altitude, 249.49205f));
+     }
+    
+    public void setVariables(int altitude){
+        this.altitude = altitude;
+    }
+    
     
 }

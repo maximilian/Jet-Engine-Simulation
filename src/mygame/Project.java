ee441318ed528fa83c1a13445acaa302bc1d782a
maximilian <2085099m@student.gla.ac.uk>
2018-01-20 15:00:27 +0000

    collecting weather data

--- src/mygame/Project.java
+++ src/mygame/Project.java
@@ -11,6 +11,21 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Line;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class Project extends SimpleApplication {
     private ResourceLoader loader;
@@ -27,7 +42,15 @@ public class Project extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        axisLines();
+        try {
+            axisLines();
+        } catch (IOException ex) {
+            Logger.getLogger(Project.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(Project.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Project.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         setDisplayFps(false);
         setDisplayStatView(false);
@@ -71,8 +94,27 @@ public class Project extends SimpleApplication {
        
     }
     
-    public void axisLines(){
-             Line xaxis = new Line(Vector3f.ZERO, new Vector3f(400f, 0, 0));
+    public void axisLines() throws MalformedURLException, IOException, SAXException, ParserConfigurationException{
+        String url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=EGPF&hoursBeforeNow=1&mostRecent=True&fields=issue_time";   
+
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setNamespaceAware(false);
+        f.setValidating(false);
+        DocumentBuilder b = f.newDocumentBuilder();
+        URLConnection urlConnection = new URL(url).openConnection();
+        urlConnection.addRequestProperty("Accept", "application/xml");
+        Document doc = b.parse(urlConnection.getInputStream());
+        doc.getDocumentElement().normalize();
+        System.out.println ("Root element: " +  doc.getDocumentElement().getAttributes().toString());
+        NodeList nList = doc.getElementsByTagName("METAR");
+        
+        Node nNode = nList.item(0);
+        Element eElement = (Element) nNode;
+        Element cElement =  (Element) eElement.getElementsByTagName("altim_in_hg").item(0);
+        System.out.println("Pressure in hg: " + cElement.getTextContent());
+    
+        System.out.println("nodes: "+nList.getLength());
+        Line xaxis = new Line(Vector3f.ZERO, new Vector3f(400f, 0, 0));
         Geometry xaxisline = new Geometry("BOOM!", xaxis);
         
         Line yaxis = new Line(Vector3f.ZERO, new Vector3f(0, 400f, 0));

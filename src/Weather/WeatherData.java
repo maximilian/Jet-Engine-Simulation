2a88a85d1f6753d44b71d7cb158e856460a4b4b9
maximilian <2085099m@student.gla.ac.uk>
2018-01-20 15:05:47 +0000

    moved code over

--- src/Weather/WeatherData.java
+++ src/Weather/WeatherData.java
@@ -5,6 +5,19 @@
  */
 package Weather;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 /**
  * Collects weather data from AviationWeather's API
  * 
@@ -12,7 +25,25 @@ package Weather;
  */
 public class WeatherData {
     
-    public void collectWeather(){
+    public void collectWeather() throws MalformedURLException, IOException, SAXException, ParserConfigurationException{
+        String url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=EGPF&hoursBeforeNow=1&mostRecent=True&fields=issue_time";   
+
+        DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+        f.setNamespaceAware(false);
+        f.setValidating(false);
+        DocumentBuilder b = f.newDocumentBuilder();
+        URLConnection urlConnection = new URL(url).openConnection();
+        urlConnection.addRequestProperty("Accept", "application/xml");
+        Document doc = b.parse(urlConnection.getInputStream());
+        doc.getDocumentElement().normalize();
+        System.out.println ("Root element: " +  doc.getDocumentElement().getAttributes().toString());
+        NodeList nList = doc.getElementsByTagName("METAR");
+        
+        Node nNode = nList.item(0);
+        Element eElement = (Element) nNode;
+        Element cElement =  (Element) eElement.getElementsByTagName("altim_in_hg").item(0);
+        System.out.println("Pressure in hg: " + cElement.getTextContent());
     
+        System.out.println("nodes: "+nList.getLength());
     }
 }

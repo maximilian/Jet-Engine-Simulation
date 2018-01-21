7e0b730b3a13b68f961d6cf07fff5fa4229102d2
maximilian <2085099m@student.gla.ac.uk>
2018-01-21 18:37:22 +0000

    date getter

--- src/Weather/WeatherData.java
+++ src/Weather/WeatherData.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+import java.time.temporal.TemporalAccessor;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -29,6 +34,8 @@ public class WeatherData {
     private float fieldTemperature;
     private String fieldName;
     
+    private LocalDateTime date;
+            
     public void collectWeather() throws MalformedURLException, IOException, SAXException, ParserConfigurationException{
         String url = "https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString=EGPF&hoursBeforeNow=1&mostRecent=True";   
         
@@ -49,15 +56,22 @@ public class WeatherData {
         Element cElement =  (Element) eElement.getElementsByTagName("altim_in_hg").item(0);
         Element tempElement =  (Element) eElement.getElementsByTagName("temp_c").item(0);
         Element stationElement =  (Element) eElement.getElementsByTagName("station_id").item(0);
+        Element timeElement =  (Element) eElement.getElementsByTagName("observation_time").item(0);
+        
         
         fieldPressure = Float.valueOf(cElement.getTextContent());
         fieldTemperature = Float.valueOf(tempElement.getTextContent());
         fieldName = stationElement.getTextContent();
         
+        String fieldTime = timeElement.getTextContent();
+        
         System.out.println("Station id is: "+fieldName);
         System.out.println("Pressure in hg: " + cElement.getTextContent());
         System.out.println("Temperature in c: " + tempElement.getTextContent());
-       
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        .withZone(ZoneId.of("UTC"));
+        date = LocalDateTime.parse(fieldTime, formatter);
 
     }
     
@@ -84,5 +98,13 @@ public class WeatherData {
         }
         
         return fieldName;
+    }
+    
+    public LocalDateTime getDateTime() throws IOException, MalformedURLException, SAXException, ParserConfigurationException{
+        if (date == null){
+            collectWeather();
+        }
+        
+        return date;
     }
 }

dcfddcca8a0e0eb43dbdb18ce188569da852f7c9
maximilian <2085099m@student.gla.ac.uk>
2018-03-25 20:48:58 +0100

    changed one word

--- src/Weather/WeatherData.java
+++ src/Weather/WeatherData.java
@@ -18,7 +18,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
- * Object for the system's weather
+ * Represents the system's weather
  * 
  * @author Maximilian Morell
  */
